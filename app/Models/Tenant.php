2a00f716fa5e35dbeb52b9486fbbce610fe98f29
Saymon Rodrigues <saymonlee25@hotmail.com>
2025-05-21 12:45:05 -0300

    created tenants-table

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Stancl\Tenancy\Database\Models\Tenant as TenantBase;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
 
-class Tenant extends TenantBase
+class Tenant extends TenantBase implements TenantWithDatabase
 {
-    use HasFactory;
+    use HasFactory, HasDatabase, HasDomains;
 }
